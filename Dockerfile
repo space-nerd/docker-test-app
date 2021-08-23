e4a7a1753a88b186ab25789568a6d86042396c1e
Austin <79015345+space-nerd@users.noreply.github.com>
2021-08-23 12:18:29 -0400

    app now uses yarn

--- Dockerfile
+++ Dockerfile
@@ -14,4 +14,4 @@ ENV PORT=3000
 
 EXPOSE 3000
 
-CMD [ "npm", "start" ]
+CMD [ "yarn", "start" ]
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ app.get('/', (_req, res) => {
     res.send("Hello, World!")
 })
 
+app.get('/circleci', (_req, res) => {
+    res.send("CirclCI")
+})
+
 app.listen(3000, () => {
     console.log(`app listening at http://localhost:3000`)
 })
